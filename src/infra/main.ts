747c783c3bd44fc8e5e85cf44975a60c81d9e646
Cassiano Junior <cassianojuniorww@gmail.com>
2023-09-30 16:15:11 -0300

    chore: enable cors

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -5,6 +5,10 @@ import { EnvService } from './env/env.service'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+  })
+
   const configService = app.get(EnvService)
 
   const port = configService.get('PORT')
