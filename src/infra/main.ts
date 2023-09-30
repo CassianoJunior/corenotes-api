2fda06bfdeecb0b5af6ce063eff5c0f196bb0b7c
Cassiano Junior <cassianojuniorww@gmail.com>
2023-09-30 18:44:27 -0300

    feat: enable cors to prod front

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://corenotes-web.vercel.app'],
   })
 
   const config = new DocumentBuilder()
